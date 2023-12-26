a67a3ea39027f97a6ddf4bf8ee7cc2fb03b1946b
User <anna.zadoiko@gmail.com>
2023-12-26 10:26:51 +0200

    Spring Data (entity, dto, repository and service)

--- src/main/java/cc/worldmandia/features/user/UserService.java
+++ src/main/java/cc/worldmandia/features/user/UserService.java
@@ -1,27 +1,47 @@
 package cc.worldmandia.features.user;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
 
-    public User getById(Long id){
-        Optional<User> user = repository.findById(id);
+    public void add(UserDto userDto){
+        User user = new User();
 
-        if (user.isEmpty()){
-            return null;
-        }
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setToken(userDto.getToken());
 
-        return user.get();
-    }
 
-    public void save(User user){
+
         repository.save(user);
     }
+
+    public List<UserDto> getList(){
+        return UserDto.from(repository.findAll());
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    public void update(UserDto userDto){
+        User user = repository.findById(userDto.getId()).orElseThrow();
+
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setToken(userDto.getToken());
+
+        repository.save(user);
+    }
+
+
 }
 

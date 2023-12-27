b68d2b074b4193dc8fb0726a05103fc1d51f8401
mani1232 <perederkoura@gmail.com>
2023-12-27 20:43:13 +0100

    refactored database structure

---
+++ src/main/java/cc/worldmandia/database/repository/URLRepository.java
@@ -0,0 +1,18 @@
+package cc.worldmandia.database.repository;
+
+import cc.worldmandia.database.entity.URL;
+import cc.worldmandia.database.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface URLRepository extends JpaRepository<URL, Long> {
+
+    @Query("SELECT u FROM URL u JOIN FETCH u.users WHERE u.shortUrl = :shortURL")
+    Optional<User> findURLWithUsersByShortURL(@Param("shortURL") String shortURL);
+
+}
---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,32 @@
+CREATE TABLE urls
+(
+    id           BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title        VARCHAR(255),
+    description  VARCHAR(255),
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    end_at       TIMESTAMP NULL,
+    enabled      BOOLEAN   DEFAULT TRUE,
+    click_count  INT       DEFAULT 0,
+    short_url    VARCHAR(10),
+    full_url     VARCHAR(255)
+);
+
+CREATE TABLE users
+(
+    id           BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    display_name VARCHAR(255),
+    username     VARCHAR(50) UNIQUE,
+    password     VARCHAR(100),
+    token        VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE users_urls
+(
+    user_id BIGINT NOT NULL,
+    url_id  BIGINT NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (url_id) REFERENCES urls (id) ON DELETE CASCADE,
+
+    UNIQUE (user_id, url_id)
+);

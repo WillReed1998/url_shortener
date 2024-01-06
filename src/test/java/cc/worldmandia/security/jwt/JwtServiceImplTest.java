66fd8bdcca99edc1e745a01491d8ba869d32ed60
smagles <lesialist@icloud.com>
2024-01-06 22:07:06 +0100

    Create JwtServiceImplTest

---
+++ src/test/java/cc/worldmandia/security/jwt/JwtServiceImplTest.java
@@ -0,0 +1,70 @@
+package cc.worldmandia.security.jwt;
+
+import cc.worldmandia.security.auth.CustomUserDetails;
+import cc.worldmandia.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JwtServiceImplTest {
+    @Value("${token.secret.key}")
+    private String jwtSecretKey;
+    @InjectMocks
+    private JwtServiceImpl jwtService;
+    @Mock
+    private CustomUserDetails userDetails;
+    private static final String TEST_VALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9" +
+            ".eyJzdWIiOiJleGFtcGxlOEBtYWlsLmNvbSIsImlhdCI6MTcwNDUzNzAyNH0" +
+            ".UPQe_0m9mIKxpFdFzNCHByDmSJr8LUqsm-oQ3-aTyYw";
+    private static final String TEST_INVALID_TOKEN = "eyJhbGciOiJIUzI1NiJ9." +
+            "eyJzdWIiOiJleGFtcGxlMUBnbWFpbC5jb20iLCJpYXQiOjE3MDQ0MDMzMTZ9." +
+            "BBIC2stCkuRiL9u6DeyOjdgRWHHCrLLdv1_bJ89-MAM";
+    private static final String TEST_PASSWORD = "Test33pass";
+    private static final String TEST_USERNAME = "example8@mail.com";
+
+    @BeforeEach
+    public void init() {
+        User user = new User();
+        user.setEmail(TEST_USERNAME);
+        user.setPassword(TEST_PASSWORD);
+        userDetails = new CustomUserDetails(user);
+
+        ReflectionTestUtils.setField(jwtService, "jwtSecretKey", jwtSecretKey);
+    }
+
+    @Test
+    void testExtractUserName() {
+        String extractedUserName = jwtService.extractUserName(TEST_VALID_TOKEN);
+        assertEquals(TEST_USERNAME, extractedUserName);
+    }
+
+    @Test
+    void testGenerateToken() {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("key1", "value1");
+        String generatedToken = jwtService.generateToken(extraClaims, userDetails);
+        assertNotNull(generatedToken);
+    }
+
+    @Test
+    void testIsTokenValid() {
+        String expiredToken = TEST_VALID_TOKEN;
+        assertTrue(jwtService.isTokenValid(expiredToken, userDetails));
+    }
+
+    @Test
+    void isTokenValid_InvalidToken() {
+        String invalidToken = TEST_INVALID_TOKEN;
+        assertFalse(jwtService.isTokenValid(invalidToken, userDetails));
+    }
+}
